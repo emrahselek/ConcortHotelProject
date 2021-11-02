02c56868acc9a371a312de8663fa6a4086c250e2
Abdas13 <abdas1349@gmail.com>
2021-11-02 10:31:10 -0400

    new

--- src/test/java/concortHotel/pages/EmrahPage.java
+++ src/test/java/concortHotel/pages/EmrahPage.java
@@ -3,5 +3,6 @@ package concortHotel.pages;
 public class EmrahPage {
     public static void main(String[] args) {
         System.out.println("emrah");
+        System.out.println("seni seviyorum emrah");
     }
 }
