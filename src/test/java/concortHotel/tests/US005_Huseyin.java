377822489971303f25695671c36f20c23f19c54b
HuseyinOzkaya <88347368+HuseyinOzkaya@users.noreply.github.com>
2021-11-03 00:24:04 -0400

    huseyin

--- src/test/java/concortHotel/tests/US005_Huseyin.java
+++ src/test/java/concortHotel/tests/US005_Huseyin.java
@@ -3,39 +3,32 @@ package concortHotel.tests;
 import concortHotel.pages.GoogleMain;
 import concortHotel.utilities.Driver;
 import concortHotel.utilities.ReusableMethods;
-
 import org.openqa.selenium.Keys;
 import org.testng.Assert;
-import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
-public class US003_Huseyin {
-    @Test()
+public class US005_Huseyin {
+    @Test(skipFailedInvocations = true)
     public void deneme() throws IOException {
         Driver.getDriver().get("https://www.google.com/");
         GoogleMain googleMain = new GoogleMain();
-        Reporter.log("We used Google Chrome Ver 80 for this test");
-        String testTitle = "Free QA Automation Tools For Everyone";
-        googleMain.googleSearchBox.sendKeys("iphone"+ Keys.RETURN);
-        boolean isTrue = Driver.getDriver().getTitle().contains("iphone");
-        if(!isTrue){
+        googleMain.googleSearchBox.sendKeys("iphone" + Keys.RETURN);
+        boolean isTrue = Driver.getDriver().getTitle().contains("huseyin");
+        if (!isTrue) {
             ReusableMethods.getScreenshot("huseyin");
             System.out.println("Title assertion doesn't match");
         }
         Assert.assertTrue(isTrue);
 
         Driver.getDriver().get("https://www.google.com/");
-        googleMain.googleSearchBox.sendKeys("huseyin"+ Keys.RETURN);
+        googleMain.googleSearchBox.sendKeys("iphone" + Keys.RETURN);
         boolean isOk = Driver.getDriver().getTitle().contains("huseyin");
-        if(!isOk){
+        if (!isOk) {
             ReusableMethods.getScreenshot("iphone");
             System.out.println("Title assertion doesn't match");
         }
         Assert.assertTrue(isOk);
-
-
-
     }
 }
