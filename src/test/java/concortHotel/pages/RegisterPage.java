628f9459ea6069e8c6c6f3669b540c703456e464
HuseyinOzkaya <88347368+HuseyinOzkaya@users.noreply.github.com>
2021-11-04 19:51:06 -0400

    huseyin

--- src/test/java/concortHotel/pages/RegisterPage.java
+++ src/test/java/concortHotel/pages/RegisterPage.java
@@ -1,6 +1,8 @@
 package concortHotel.pages;
 
 import concortHotel.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class RegisterPage {
@@ -9,5 +11,43 @@ public class RegisterPage {
         PageFactory.initElements(Driver.getDriver(),this);
 
     }
+    @FindBy (id = "UserName")
+    public WebElement userName;
+
+    @FindBy (id = "Password")
+    public WebElement password;
+
+    @FindBy (id = "Email")
+    public WebElement email;
+
+    @FindBy (id = "NameSurname")
+    public WebElement fullName;
+
+    @FindBy (id = "PhoneNo")
+    public WebElement phoneNo;
+
+    @FindBy (id = "SSN")
+    public WebElement socialSecurityNumber;
+
+    @FindBy (id = "DrivingLicense")
+    public WebElement drivingLicenseNo;
+
+    @FindBy (id = "IDCountry")
+    public WebElement idCountry;
+
+    @FindBy (id = "IDState")
+    public WebElement idState;
+
+    @FindBy (id = "Address")
+    public WebElement address;
+
+    @FindBy (id = "WorkingSector")
+    public WebElement workingSector;
+
+    @FindBy (id = "BirthDate")
+    public WebElement birthDate;
+
+    @FindBy (id = "btnSubmit")
+    public WebElement saveButton;
 
 }
--- src/test/java/concortHotel/tests/US003_Huseyin.java
+++ src/test/java/concortHotel/tests/US003_Huseyin.java
@@ -1,41 +1,39 @@
 package concortHotel.tests;
 
 import concortHotel.pages.GoogleMain;
+import concortHotel.pages.LoginPage;
+import concortHotel.pages.MainPage;
+import concortHotel.utilities.ConfigReader;
 import concortHotel.utilities.Driver;
 import concortHotel.utilities.ReusableMethods;
 
 import org.openqa.selenium.Keys;
 import org.testng.Assert;
 import org.testng.Reporter;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
 public class US003_Huseyin {
-    @Test()
-    public void deneme() throws IOException {
-        Driver.getDriver().get("https://www.google.com/");
-        GoogleMain googleMain = new GoogleMain();
-        Reporter.log("We used Google Chrome Ver 80 for this test");
-        String testTitle = "Free QA Automation Tools For Everyone";
-        googleMain.googleSearchBox.sendKeys("iphone"+ Keys.RETURN);
-        boolean isTrue = Driver.getDriver().getTitle().contains("oppps");
-        if(!isTrue){
-            ReusableMethods.getScreenshot("huseyin");
-            System.out.println("Title assertion doesn't match");
-        }
-        Assert.assertTrue(isTrue);
 
-        Driver.getDriver().get("https://www.google.com/");
-        googleMain.googleSearchBox.sendKeys("huseyin"+ Keys.RETURN);
-        boolean isOk = Driver.getDriver().getTitle().contains("huseyin");
-        if(!isOk){
-            ReusableMethods.getScreenshot("iphone");
-            System.out.println("Title assertion doesn't match");
-        }
-        Assert.assertTrue(isOk);
+    MainPage mainPage;
+    LoginPage loginPage;
+
+    @BeforeMethod
+    public void setUp(){
+        Driver.getDriver().get(ConfigReader.getProperty("app_url_qa"));
+        mainPage = new MainPage();
+        loginPage = new LoginPage();
+        mainPage.logInLink.click();
+        loginPage.createNewAccount.click();
+    }
+
+    @Test
+    public void tc_0001(){
 
 
 
     }
+
 }
